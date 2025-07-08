52b584b3fcea9f27acc35961dc89d2fa9cba06bd
Daniel J. Felipe Feliz Santana <113933957+DanielFelizS@users.noreply.github.com>
2025-07-07 22:13:46 -0400

    Update HomePage.tsx
    El proyecto lo he creado yo solo, habia agregado al resto de pasantes solo porque debiamos entregar el proyecto y terminar la pasantia

--- Inventario2.0/src/components/pages/Home/HomePage.tsx
+++ Inventario2.0/src/components/pages/Home/HomePage.tsx
@@ -7,27 +7,13 @@ export default function HomePage() {
       <Navigation />
       <h1>Acerda de</h1>
 
-      <div id="contenedor">
-        <Pasantes
-          nombre="Leonardo"
-          apellido="Rodriguez Molina"
-          IconName="Leonardo"
-          personaImg="../img/LeonardoRM.jpeg"
-          number="+1 (829) 927-4971"
-        />
+      <div id="contenedor" class="container">
         <Pasantes
           nombre="Daniel J. Felipe"
           apellido="Féliz Santana"
           IconName="Daniel"
           personaImg="../img/DanielFS.jpeg"
           number="+1 (849) 446-9260"
-        />
-        <Pasantes
-          nombre="Francis Enmanuel"
-          apellido="Ramirez Perez"
-          IconName="Francis"
-          personaImg="../img/FrancisRP.jpeg"
-          number="+1 (829) 768-1205"
         />
       </div>
     </>
